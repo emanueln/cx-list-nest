1ccd1ae35e016431d9de22903a06acd601829eb8
Emanuel Neves <emanuel.neves@protonmail.com>
2019-02-28 23:06:16 -0500

    add service and controller for customer

--- customer-list-app-backend/src/customer/customer.module.ts
+++ customer-list-app-backend/src/customer/customer.module.ts
@@ -1,4 +1,15 @@
-import { Module } from '@nestjs/common';
+// ./src/customer/customer.module.ts
 
-@Module({})
-export class CustomerModule {}
+import { Module } from '@nestjs/common';
+import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerSchema } from './schemas/customer.schema';
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Customer', schema: CustomerSchema }])
+  ],
+  controllers: [CustomerController],
+  providers: [CustomerService]
+})
+export class CustomerModule { }
